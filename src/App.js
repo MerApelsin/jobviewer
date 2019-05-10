99007e6ffb4fa271985363d1903eecb3c38c3180
MerApelsin <cecilia.carlsson93@gmail.com>
2019-05-10 11:23:39 +0200

    initial test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
+import DataHandler from './components/data/dataHandler.js';
 
 //main flow + callbacks + router?
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    hasFetched: false,
+    status: ''
+  }
+
+  componentDidMount() {
+     console.log(DataHandler.fetchData());
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/data/dataHandler.js
+++ src/components/data/dataHandler.js
@@ -8,5 +8,44 @@
 
 //get stuff for 'ad'
 
-//singleton/shared instance?
+class DataHandler{
+    constructor()
+    {
+        this.data = {};
+      //firebase.initializeApp(config);
+      //this.auth = firebase.auth();
+    }
+
+    /*signIn = (email,password) => {
+      return this.auth.signInWithEmailAndPassword(email,password);
+    }*/
+
+    fetchData = async () => {
+        try{
+            const rawData = await fetch('https://feed.jobylon.com/feeds/7d7e6fd12c614aa5af3624b06f7a74b8/?format=json');
+            let fetchedData = await JSON.parse(rawData);
+            console.log(fetchedData);
+        }
+        catch(err){
+            return err;
+        }
+        
+    }
+  }
+  /*
+  function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+  */
+
+  const dataHandler = new DataHandler();
+
+  //exponerar bara den faktiskta instansen av firebase vilket gör att andra filer inte kan/ska initisera firebase på flera ställen
+  export default DataHandler;
+
 
