14a2c2d112e5eb583ffbd258c040cf4add273fff
MerApelsin <cecilia.carlsson93@gmail.com>
2019-05-12 19:06:59 +0200

    Updates

---
+++ precache-manifest.8c835000b36c0b8d000c17c745aa99d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48908fbf92e413ebf61ce919fb9e3f8a",
+    "url": "/jobviewer/index.html"
+  },
+  {
+    "revision": "57eebff223bdf9cd4cb0",
+    "url": "/jobviewer/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9c272464acae76732fd9",
+    "url": "/jobviewer/static/css/main.66ecdc65.chunk.css"
+  },
+  {
+    "revision": "57eebff223bdf9cd4cb0",
+    "url": "/jobviewer/static/js/2.8eee408b.chunk.js"
+  },
+  {
+    "revision": "9c272464acae76732fd9",
+    "url": "/jobviewer/static/js/main.756dc29e.chunk.js"
+  },
+  {
+    "revision": "22e38e532add7499a296",
+    "url": "/jobviewer/static/js/runtime~main.218bf7d4.js"
+  },
+  {
+    "revision": "4b6cb86267be5ba80c329c52bf0dd487",
+    "url": "/jobviewer/static/media/coverPlaceholder.4b6cb862.jpg"
+  },
+  {
+    "revision": "ba69dda1a275148585df6c5d1447819c",
+    "url": "/jobviewer/static/media/header.ba69dda1.jpg"
+  }
+]);
